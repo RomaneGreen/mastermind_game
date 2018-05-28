cd1e2af1dc3dde0496a670fb3944d0a04ae2ba60
Romane <romane71193@gmail.com>
2018-05-28 13:44:09 -0400

    added initialization + instance variables

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,4 +1,5 @@
 
+
 puts  %{
  __ __  __    __ _____ ___ ___ __ __ _ __  _ __
 |  V  |/  \ /' _/_   _| __| _ \  V  | |  \| | _\
@@ -8,100 +9,116 @@ puts  %{
 }
 sleep(1)
 
+class Gamecpu
 
-class Game
+def initialize()
+@computer_mind = ['red','orange','yellow','green','blue','black','brown','white','purple']
+
+@computer_guess = []
+@secret_code = []
+@updated_code = []
+end
 
 
 def cpuplay
 
-computer_mind = ['red','orange','yellow','green','blue','black','brown','white','purple']
 
-computer_guess = []
-secret_code = []
-updated_code = []
 
 puts "4 colors,for your secret code"
 puts "Enter color 1"
 answer = gets.strip.downcase
-secret_code.push(answer)
+@secret_code.push(answer)
 
 puts "Enter color 2"
 answer = gets.strip.downcase
-secret_code.push(answer)
+@secret_code.push(answer)
 
 puts "Enter color 3"
 answer = gets.strip.downcase
-secret_code.push(answer)
+@secret_code.push(answer)
 
 puts "Enter color 4"
 answer = gets.strip.downcase
-secret_code.push(answer)
+@secret_code.push(answer)
 puts "\n"
 
-until updated_code.size == secret_code.size || computer_guess.length > 12
+until @updated_code.size == @secret_code.size || @computer_guess.length > 12
 p "computer is guessing..."
 sleep(3)
-cpuguess = computer_mind.sample
+cpuguess = @computer_mind.sample
 
 puts "computer guessed #{cpuguess}"
 puts "\n"
-computer_guess.push(cpuguess)
+@computer_guess.push(cpuguess)
 
-if updated_code.include? cpuguess
+if @updated_code.include? cpuguess
 puts "Sorry cpu... #{cpuguess} already guessed!"
 puts "\n"
 
-elsif secret_code.include? cpuguess
+elsif @secret_code.include? cpuguess
 puts "cpu guess of #{cpuguess} is correct!"
 puts "\n"
-updated_code.push(cpuguess)
-p updated_code
+@updated_code.push(cpuguess)
+p @updated_code
 
 else
 puts "Cpu guess of #{cpuguess} is wrong!"
 puts "\n"
-p updated_code
+p @updated_code
 
 end
 end
 
-if updated_code.length == 4
+if @updated_code.length == 4
 puts "CPU has guessed all codes and won!"
 
-elsif computer_guess.length > 12
+elsif @computer_guess.length > 12
 puts "CPU has lost! no more guesses left"
 
 end
 
 end
+end
 
+
+
+class Gamehuman
+
+attr_accessor :colors , :colorsguess , :chances
+
+
+def initialize()
+
+@colors = ['red','green','blue','yellow']
+@colorsguess = []
+@chances = 0
+
+end
 
 def humanplay
-colors = ['red','green','blue','yellow']
-colorsguess = []
-chances = 0
+
 puts "Welcome to Mastermind..."
 puts "Can you guess the four colors?"
 puts "____ ____ ____ ____ \n"
 puts ""
 
-until chances == 10 || colorsguess.size == colors.size
+until @chances == 10 || @colorsguess.size == @colors.size
 answer = gets.strip.downcase
 
-if colorsguess.include? answer
-puts "already guessed,try again"
-chances += 1
-elsif colors.include? answer
+if @colorsguess.include? answer
+puts "already guessed dickhead"
+@chances += 1
+elsif @colors.include? answer
   puts "Correct..guess another color
-   #{3 - colorsguess.size} colors to guess left! \n "
-  colorsguess.push(answer)
+   #{3 - @colorsguess.size} colors to guess left! \n "
+  @colorsguess.push(answer)
 
-  p colorsguess
-  chances += 1
+  p @colorsguess
+  @chances += 1
 else
   puts "Wrong try again!"
-  p colorsguess
-  chances +=1
+  p @colorsguess
+  @chances +=1
   case answer
 
   when 'brown'
@@ -121,9 +138,9 @@ end
 
 end
 
-if colorsguess.size == colors.size
+if @colorsguess.size == @colors.size
   puts " \n All colors guessed! Congrats you've won!"
-elsif chances >= 10
+elsif @chances >= 10
   puts "no more guesses left! GAME OVER"
 
 end
@@ -131,6 +148,7 @@ end
 end
 
 end
+
 
 
 
@@ -142,9 +160,9 @@ puts "\n"
 reply = gets.strip.downcase.to_i
 
 if reply == 1
- G = Game.new
- G.cpuplay
-elsif reply == 2
-  G = Game.new
+ G = Gamehuman.new
   G.humanplay
+elsif reply == 2
+  G = Gamecpu.new
+ G.cpuplay
 end
